a74fd1d3aedb4476cc4792dea3eb809f4390552f
mesutgdk <mesuttgedik@gmail.com>
2023-08-29 22:00:20 +0300

    feat: pickerview and button is added

--- task2_app/task2_app/AlarmViewController.swift
+++ task2_app/task2_app/AlarmViewController.swift
@@ -8,24 +8,69 @@
 import UIKit
 
 class AlarmViewController: UIViewController {
-
-    var datePicker: UIPickerView?
+    
+    var datePicker: UIPickerView = {
+        var picker = UIPickerView()
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        return picker
+    }()
+    
+    let setButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.configuration = .filled()
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 25
+        button.backgroundColor = .systemIndigo
+        button.setTitle("Set alarm", for: [])
+        button.addTarget(self, action: #selector(setButtonTapped), for: .primaryActionTriggered)
+        
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setup()
+        layout()
+    }
+    private func setup (){
+        view.addSubviews(datePicker,setButton)
+        view.backgroundColor = .white
+    }
+    private func layout() {
+        // datePicker
+        NSLayoutConstraint.activate([
+            datePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
+            datePicker.heightAnchor.constraint(equalToConstant: 100),
+            datePicker.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            datePicker.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20)
+        ])
+        // setButton
+        NSLayoutConstraint.activate([
+            setButton.heightAnchor.constraint(equalToConstant: 50),
+            setButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 100),
+            setButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50),
+            setButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -50)
+        ])
     }
     
 }
+// MARK: - Action
+extension AlarmViewController{
+    @objc func setButtonTapped(){
+        
+    }
+}
+// MARK: -PickerView Delegate and DataSourse
 extension AlarmViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        return 2
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        <#code#>
+        return 30
     }
-    
     
 }
 
